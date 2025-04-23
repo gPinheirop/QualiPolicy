80500b95969ef691815f7a177b168fbf6d03bc5f
gPinheirop <gabrielpedreira_@hotmail.com>
2025-04-23 10:44:09 -0300

    :tada: Initial windows setup

---
+++ windows/QualiPolice/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::QualiPolice::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::QualiPolice::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
