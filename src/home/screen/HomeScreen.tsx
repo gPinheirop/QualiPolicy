6899dfcdfb5994b4236aacee70301cc937937b71
gPinheirop <gabrielpedreira_@hotmail.com>
2025-04-30 08:45:32 -0300

    :lipstick: Update home page

--- src/home/screen/HomeScreen.tsx
+++ src/home/screen/HomeScreen.tsx
@@ -1,23 +1,22 @@
-import {Image, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View className="w-full h-full bg-red-400 flex items-center justify-center">
-      <View className="h-1/2 w-2/5 p-16 bg-gray-200 rounded-3xl flex flex-col items-center justify-between gap-2">
+      <View className="h-1/2 w-2/5 p-16 bg-gray-200 rounded-3xl flex flex-col items-center justify-center gap-8">
         <Image
           source={require('../../assets/images/logo.png')}
           className="rounded-lg max-h-60"
           resizeMode="contain"
         />
-        <View className="flex flex-col gap-1 w-4/5">
-          <Text className="color-black text-lg">Email</Text>
-          <TextInput className="bg-white rounded-lg text-black h-12" />
-        </View>
-        <View className="flex flex-col gap-1 w-4/5">
-          <Text className="color-black text-lg ">Senha</Text>
-          <TextInput className="bg-white rounded-lg text-black h-12" />
-        </View>
-        <TouchableOpacity className="bg-red-500 w-[45%] rounded-lg px-8 py-4 flex items-center">
+        <TouchableOpacity
+          className="bg-red-500 w-[45%] rounded-lg px-8 py-4 flex items-center"
+          onPress={() => {
+            navigation.navigate('Report');
+          }}>
           <Text>Entrar</Text>
         </TouchableOpacity>
       </View>
